c93915fbfcf736a8774e8ad18031211f88e85c87
Alexander <alexander19.evsyukov97@gmail.com>
2022-02-02 16:07:23 +0300

    cycle_while

---
+++ Cycle_while/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму четных чисел от 1 до 15
+Console.Clear();
+int a = 0;
+int s = 0;
+while (a < 15)
+{
+    a = a + 1; //счетчик
+    if (a % 2 == 0) s = s + a; //сумматор
+    System.Console.WriteLine(a);
+}
+Console.WriteLine(s);
