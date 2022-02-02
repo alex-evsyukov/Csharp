e05847397db828881669207127a72b92bd9e291f
Alexander <alexander19.evsyukov97@gmail.com>
2022-02-02 15:04:56 +0300

    data types integer

--- datatypes/Program.cs
+++ datatypes/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// С клавиатуры вводятся два числа. Подсчитатьих сумму
+int a = 1;
+int b = 2;
+int sum = 0;
+string s;
+Console.Clear();
+Console.Write("Введите число a: ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.Write("Введите число b: ");
+s = Console.ReadLine();
+b = Convert.ToInt32(s);
+sum = a + b;
+System.Console.WriteLine($"{a} + {b} = {sum}");
