cf43742ebc5556ba858fe25dec67f1ebb05f1644
Alexander <alexander19.evsyukov97@gmail.com>
2022-02-02 15:09:32 +0300

    condition if else

---
+++ Condition_if_else/Program.cs
@@ -0,0 +1,32 @@
+﻿//Найти максимальное из 2 чисел
+double a, b, max = 0;
+string s;
+
+Console.Clear();
+Console.Write("Введите число a: ");
+s = Console.ReadLine();
+a = Convert.ToDouble(s);
+
+Console.Write("Введите число b: ");
+s = Console.ReadLine();
+b = Convert.ToDouble(s);
+
+// Не полная форма условного оператора
+// if (a > b) max = a;
+// if (b > a) max = b;
+// if (a == b) max = a;
+
+// Полная форма условного оператора
+//if (a > b) max = a; else max = b;
+
+if (a > b)
+{
+    max = a;
+    System.Console.WriteLine();
+}
+else
+{
+    max = b;
+}
+
+System.Console.WriteLine($"max = {max}");
